20df82ccf8c2c443cbc006caffd4969332208cb5
Luis Trigueiros <luistrigueiros@gmail.com>
2019-01-01 20:49:03 +0000

    Tidy up more Koin like

--- src/test/kotlin/vertx/protobuffer/message/codec/Receiver.kt
+++ src/test/kotlin/vertx/protobuffer/message/codec/Receiver.kt
@@ -5,13 +5,14 @@ import io.vertx.core.eventbus.EventBus
 import org.koin.log.Logger.SLF4JLogger
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.StandAloneContext.startKoin
-import org.koin.standalone.get
+import org.koin.standalone.inject
 
 object Receiver : KoinComponent {
+    private val eventBus : EventBus by inject()
+
     @JvmStatic
     fun main(args: Array<String>) {
         startKoin(listOf(TestModule.vertModule), logger = SLF4JLogger())
-        val eventBus = get<EventBus>()
         eventBus.consumer<Example.GeoPoint>(POINTS_TO_VISIT) {
             println("Got =====> " + it.body())
         }
--- src/test/kotlin/vertx/protobuffer/message/codec/Sender.kt
+++ src/test/kotlin/vertx/protobuffer/message/codec/Sender.kt
@@ -5,14 +5,16 @@ import io.vertx.core.eventbus.EventBus
 import org.koin.log.Logger.SLF4JLogger
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.StandAloneContext.startKoin
-import org.koin.standalone.get
+import org.koin.standalone.inject
 
 object Sender : KoinComponent {
+    private val vertx:Vertx by inject()
+    private val eventBus : EventBus by inject()
+
     @JvmStatic
     fun main(args: Array<String>) {
         startKoin(listOf(TestModule.vertModule), logger = SLF4JLogger())
-        val eventBus = get<EventBus>()
-        get<Vertx>().setPeriodic(1000) {
+        vertx.setPeriodic(1000) {
             eventBus.send(POINTS_TO_VISIT, DUBLIN)
         }
         Thread.sleep(Long.MAX_VALUE)
